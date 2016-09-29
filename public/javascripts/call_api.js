e4e9e433490eab846c576908c594b06304b71b8e
Jackson Wearn <jacksonwearn@gmail.com>
2016-09-29 15:23:24 -0400

    Pull in all possible input values and send using AJAX

--- public/javascripts/call_api.js
+++ public/javascripts/call_api.js
@@ -1,17 +1,32 @@
 var callAPI = function() {
   var url = $("input#api-route").val();
+  var method = $("select#method").val();
+  var contentType = $("select#content-type").val();
+  var accept = $("select#accept").val();
+  var version = $("input#version").val();
+  var headers = $("input#headers").val();
+  var body = $("textarea#body").val();
+
+  var payload = {
+    "url": url,
+    "method": method,
+    "contentType": contentType,
+    "accept": contentType,
+    "version": version,
+    "headers": headers,
+    "body": body
+  };
+  
   $.ajax({
     url: "/callapi",
     type: "POST",
-    data: JSON.stringify({
-      "url": url
-    }),
+    data: JSON.stringify(payload),
     contentType:"application/json; charset=utf-8",
     cache: false,
     success: function(data, textStatus, jqXHR) {
       console.log(data);
-      console.log(textStatus);
-      console.log(jqXHR);
+      var response = JSON.stringify(JSON.parse(data),null,2);
+      $("pre#response-area").html((response));
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(jqXHR);
@@ -19,5 +34,4 @@ var callAPI = function() {
       console.log(errorThrown);
     }
   });
-};
-
+}
