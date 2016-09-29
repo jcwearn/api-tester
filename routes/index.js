5d9a385351e211757e1eec31c9c16b423f923fba
Jackson Wearn <jacksonwearn@gmail.com>
2016-09-29 15:23:54 -0400

    Pass req.body instead of trying to pase at params

--- routes/index.js
+++ routes/index.js
@@ -12,30 +12,21 @@ router.post('/callapi', function(req, res, next) {
   var statusCode = 500;
   var response;
   if(req.body) {
-    if (req.body.url && req.body.method) {
-      var url = req.body.url;
-      var method = req.body.method;
-      var headers = req.body.headers;
-      var body = req.body.body;
-      APIController.callAPI(url, method, function(err, data) {
-        if (err) {
-          console.log(err);
-          response = err.text;
-        } else if (data) {
-          statusCode = 200;
-          response = data;
-        } else {
-          statusCode = 400;
-          response = "No response from API!";
-        }
-      });
-    }
-  } else {
-    statusCode = 400;
-    response = "Missing API URL or request Method Type!";
+    var controller = new APIController();
+    controller.callAPI(req.body, function(err, data) {
+      if (err) {
+        console.log(err);
+        response = err.text;
+      } else if (data) {
+        statusCode = 200;
+        response = data;
+      } else {
+        statusCode = 400;
+        response = "No response from API!";
+      }
+      res.status(statusCode).send(response);
+    });
   }
-
-  res.status(statusCode).send(response);
 });
 
 module.exports = router;
