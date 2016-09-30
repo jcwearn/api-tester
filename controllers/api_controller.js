d8d57cdfabd91fe4e12eb90b11967d12d97f4558
Jackson Wearn <jacksonwearn@gmail.com>
2016-09-29 20:47:36 -0400

    Try to parse body.  If not valid json callback with error. Callback with response on success.  This way the body and headers can be displayed in frontend

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -4,7 +4,6 @@ var APIController = function() {};
 
 
 APIController.prototype.callAPI = function(api_params, callback) {
-  
   var headers = {
     "accept": api_params.accept,
     "content-type": api_params.contentType,
@@ -25,14 +24,19 @@ APIController.prototype.callAPI = function(api_params, callback) {
   };
 
   if (api_params.body && (request_params.method === "POST" || request_params.method === "PUT")) {
+    try {
+      var body = JSON.parse(api_params.body);
+    } catch (e) {
+      return callback("Error parsing JSON body!");
+    }    
     request_params.body = api_params.body;
   }
 
   request(request_params, function(error, response, body) {
     if (error) {
       return callback(error, null);
-    } else if(body) {
-      return callback(null, body);
+    } else if(response) {
+      return callback(null, response);
     } else {
       return callback(new Error("No Results!"), null);
     }
