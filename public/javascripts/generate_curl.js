432d185eb46e5ee228582afd3be067bd75da5332
Jackson Wearn <jacksonwearn@gmail.com>
2016-09-30 09:02:13 -0400

    Function to generate cURL script based on content in forms on page

---
+++ public/javascripts/generate_curl.js
@@ -0,0 +1,28 @@
+var generateCURL = function() {
+  var formContent = grabFormContent();
+  var url = formContent.url;
+  var method = formContent.method;
+  var contentType = formContent.contentType;
+  var accept = formContent.accept;
+  var version = formContent.version;
+  var headers = formContent.headers;
+  var body = formContent.body;
+  
+  var cURL = "curl -X " + method +  " -H \"" + "Content-Type"  + ":" + contentType + "\"" + " -H \"" + "Accept"  + ":" + accept + "\"";
+
+  if (version) {
+    
+  }
+
+  if (headers) {
+    
+  }
+
+  if (body) {
+    cURL += "-d '" + body + "' ";
+  }
+
+  cURL += " \"" + url + "\"";
+
+  $("code#curl-area").html(cURL);
+};
